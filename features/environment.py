7487e99aee45fe8ad96d1d3a99149edccd52387f
Cristian Lujan <cxristian.lujan05@gmail.com>
2019-04-18 16:19:48 -0400

    merging classes

--- features/environment.py
+++ features/environment.py
@@ -18,7 +18,7 @@ def before_scenario(context, scenario):
     elif ("get_stories" in scenario.tags):
         logger.info("Get all project and get the id of the project")
         context.ids = [Project_Helper.get_all_projects()[0]['id']]
-    elif ("get" and "stories" and "bullk" in scenario.tags):
+    elif ("get_stories" in scenario.tags):
         logger.info("Get all project and get the id of the project")
         context.ids = [Project_Helper.get_all_projects()[0]['id']]
         context.ids.append(Stories_helper.get_all_Story_by_Id(context.ids[0])[0]['id'])
