3c2a0143f62113dd4c3fc091de32511b1299c553
melvicaballero <melvi.caballero@gmail.com>
2019-04-18 12:26:20 -0400

    add proj_id to endpoint epic

--- core/utils/endpoint_helper.py
+++ core/utils/endpoint_helper.py
@@ -4,5 +4,9 @@ class EndpointHelper:
 
     @staticmethod
     def translate_endpoint(endpoint):
-        endpoint = endpoint.replace('{epic_id}', str(Repository.get_instance().epic_id))
+        if '{epic_id}' in endpoint:
+            endpoint = endpoint.replace('{epic_id}', str(Repository.get_instance().epic_id))
+        # if '{proj_id}' in endpoint:
+        #     endpoint = endpoint.replace('{proj_id}', project_id)
+
         return endpoint
