db239c4b7f95ab0fefd21df3208d86153cbfc4ad
Areliez Vargas <areliez.vargas@gmail.com>
2019-04-23 16:12:16 -0400

    Modify the creation the account and return account random

--- core/utils/account_helper.py
+++ core/utils/account_helper.py
@@ -1,35 +1,38 @@
 from core.rest_client.request_manager import RequestManager
+import random
+import string
+import json
 
 
 class Account_helper:
-    @staticmethod
-    def create_account():
-        client = RequestManager()
-        client.set_method("POST")
-        client.set_endpoint("/projects")
-        client.set_body(
-            """
-            {
-                "new_account_name": "my test",
-                "name": "Test333"
-            }
-            """
-        )
-        return client.execute_request().json()['account_id']
 
     @staticmethod
-    def create_account_membership(id):
+    def create_account():
+        if len(Account_helper.get_accounts()) < 5:
+            client = RequestManager()
+            client.set_method("POST")
+            client.set_endpoint("/projects")
+            client.set_body(json.dumps(Account_helper.generate_body_account()))
+            id_account = client.execute_request().json()['account_id']
+        else:
+            id_account = Account_helper.get_account_random()['id']
+        return id_account
+
+    @staticmethod
+    def create_account_membership(account_id):
         client = RequestManager()
         client.set_method("POST")
-        client.set_endpoint("/accounts/{id_account}/memberships".format(id_account=id))
+        client.set_endpoint("/accounts/{account_id}/memberships".format(account_id=account_id))
         client.set_body(
             """
             {
-                "new_account_name": "my test",
-                "name": "Test333"
+                "email":"AT09@example.com",
+                "initials":"AT",
+                "name":"Automation Testing"
             }
             """
         )
+        client.execute_request()
 
     @staticmethod
     def get_account_id():
@@ -38,3 +41,22 @@ class Account_helper:
         client.set_endpoint('/accounts')
         response = client.execute_request()
         return response.json()[0]["id"]
+
+    @staticmethod
+    def get_accounts():
+        client = RequestManager()
+        client.set_method('GET')
+        client.set_endpoint('/accounts')
+        return client.execute_request().json()
+
+    @staticmethod
+    def get_account_random():
+        account_list = Account_helper.get_accounts()
+        return random.choice(account_list)
+
+    @staticmethod
+    def generate_body_account():
+        account_name = "Account " + ''.join(random.choices(string.digits, k=4))
+        project_name = "Project " + ''.join(random.choices(string.digits, k=3))
+        body = {"new_account_name": account_name, "name": project_name}
+        return body
