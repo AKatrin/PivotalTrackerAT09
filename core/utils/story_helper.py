fdb5d0085eda8c7969a3a2159e303117b873d7f7
LuisGuardia <g.luisalberto68@gmail.com>
2019-04-28 14:33:09 -0400

    new changes story tests

--- core/utils/story_helper.py
+++ core/utils/story_helper.py
@@ -24,22 +24,25 @@ class Story_Helper:
     @staticmethod
     def put_stories(id_project, id_stories):
         '''
-        Create project
+        Update project with the labels
         :return: Project Json
         '''
         client = RequestManager()
         client.set_method('PUT')
         client.set_endpoint('/projects/' + str(id_project) + '/stories/' + str(id_stories))
-        body = {'{"labels":[{"name":"newnew"},{"name":"labellabel"},{"name":"newLabel"}]}'}
-        client.set_body(json.dumps(body))
+        client.set_body(
+            """
+            {"labels":[{"name":"test label 1"}]}
+            """
+        )
         response = client.execute_request()
-        return response.json()
+        return response.json()['labels']
 
 
     @staticmethod
     def delete_story(id_project, id_stories):
         '''
-        Delete stories of a  project
+        Delete storie of a  project
         :param response: Json
         '''
         print("Eliminando el proyecto ")
@@ -48,5 +51,56 @@ class Story_Helper:
         client.set_endpoint('/projects/' + str(id_project) + "/stories/" + str(id_stories))
         client.execute_request()
 
+    @staticmethod
+    def account_me():
+        '''
+        Get my account id
+        :return: Stories Json
+        '''
+        client = RequestManager()
+        client.set_method('GET')
+        client.set_endpoint('/me')
+        response = client.execute_request()
+        return response.json()
 
+    @staticmethod
+    def get_all_stories(id_project):
+        '''
+        Get all stories of a project
+        :param id: Integer
+        :return: Stories Json
+        '''
+        client = RequestManager()
+        client.set_method('GET')
+        client.set_endpoint('/projects/' + str(id_project) + "/stories")
+        response = client.execute_request()
+        return response.json()
 
+    @staticmethod
+    def compareProject(body, json_result):
+        answer = ""
+        body = body["labels"]
+        result = json_result["labels"]
+        if len(body) == len(result):
+            for item in body:
+                if str(type(item)).find("dict") > 0:
+                    for element in item:
+                        flag = False
+                        for jsons in result:
+                            if jsons[element] == item[element]:
+                                flag = True
+                                break
+                        if flag is False:
+                            answer = answer + "The value is not the same in " + element + ": " + item[element]
+                elif str(type(item)).find("str") > 0:
+
+                    flag = False
+                    for jsons in result:
+                        if jsons["name"] == item:
+                            flag = True
+                            break
+                    if flag is False:
+                        answer = answer + "The value is not the same in name: " + item
+        else:
+            answer = "The size of Labels of result are not the same as the body"
+        return answer
